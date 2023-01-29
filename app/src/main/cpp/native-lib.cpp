ba87baec3f4e5e96dae9d34871b924bede5ce269
Kazuki Ohara <kazuki.ohara@gmail.com>
2023-01-29 19:11:40 +0900

    use fbjni

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,10 +1,24 @@
 #include <jni.h>
 #include <string>
+#include <fbjni/fbjni.h>
 
-extern "C" JNIEXPORT jstring JNICALL
-Java_com_example_fbjniapplicatioin1_MainActivity_stringFromJNI(
-        JNIEnv* env,
-        jobject /* this */) {
-    std::string hello = "Hello from C++";
-    return env->NewStringUTF(hello.c_str());
+class MainActivity : public facebook::jni::JavaClass<MainActivity> {
+public:
+    static constexpr auto kJavaDescriptor = "Lcom/example/fbjniapplicatioin1/MainActivity;";
+
+    static std::string stringFromJNI(facebook::jni::alias_ref<facebook::jni::JClass> clazz) {
+        return std::string{"Hello from fbjni"};
+    }
+
+    static void registerNatives() {
+        javaClassStatic()->registerNatives({
+            makeNativeMethod("stringFromJNI", MainActivity::stringFromJNI)
+        });
+    }
+};
+
+extern "C" JNIEXPORT jint JNI_OnLoad(JavaVM *vm, void *reserved) {
+    return facebook::jni::initialize(vm, [] {
+        MainActivity::registerNatives();
+    });
 }
